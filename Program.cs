dc60339d96ae71c1979a4b87b9e5b50268755459
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-05-22 18:11:08 -0300

    Ejercicio 1 realizado

--- Program.cs
+++ Program.cs
@@ -6,4 +6,30 @@ a = 10;
 b = a;
 Console.WriteLine("Valor de a:"+a);
 Console.WriteLine("Valor de b:" +b);
-
+string numInvertir;
+Console.WriteLine("Ingrese un numero");
+numInvertir = Console.ReadLine();
+int longitudCadena;
+longitudCadena = numInvertir.Length;
+int aux;
+if (int.TryParse(numInvertir, out aux))
+{
+    if (aux > 0)
+    {
+        string auxiliar = "";
+        for (int i = longitudCadena - 1; i >= 0; i--)
+        {
+            auxiliar += numInvertir[i];
+        }
+        Console.WriteLine("El numero es:" + aux);
+        Console.WriteLine("El numero invertido es:" + auxiliar);
+    }
+    else
+    {
+        Console.WriteLine("El numero debe ser mayor a 0");
+    }
+}
+else
+{
+    Console.WriteLine("No ingreso un numero");
+}
