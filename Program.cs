984e6554dc730914b614d5f4e538bb1f5241e803
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-05-22 20:11:18 -0300

    calculadora v1

--- Program.cs
+++ Program.cs
@@ -1,35 +1,82 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int a;
-int b;
-a = 10;
-b = a;
-Console.WriteLine("Valor de a:"+a);
-Console.WriteLine("Valor de b:" +b);
-string numInvertir;
-Console.WriteLine("Ingrese un numero");
-numInvertir = Console.ReadLine();
-int longitudCadena;
-longitudCadena = numInvertir.Length;
-int aux;
-if (int.TryParse(numInvertir, out aux))
+int opcion;
+do
 {
-    if (aux > 0)
+    
+    int resultado;
+    int decision;
+    Console.WriteLine("Seleccione la operacion que quiera hacer:");
+    Console.WriteLine("[1] Suma de los numeros:");
+    Console.WriteLine("[2] Resta de los numeros:");
+    Console.WriteLine("[3] Multiplicacion de los numeros:");
+    Console.WriteLine("[4] Division de los numeros:");
+    Console.WriteLine("[5] salir");
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
     {
-        string auxiliar = "";
-        for (int i = longitudCadena - 1; i >= 0; i--)
-        {
-            auxiliar += numInvertir[i];
-        }
-        Console.WriteLine("El numero es:" + aux);
-        Console.WriteLine("El numero invertido es:" + auxiliar);
+        case 1:
+            do
+            {
+                Console.WriteLine("Ingrese dos numeros");
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+                resultado = num1 + num2;
+                Console.WriteLine("El resultado es:" + resultado);
+                Console.WriteLine("Desea realizar otra operacion? Ingrese 1 para si y ingrese 2 para No");
+                decision = int.Parse(Console.ReadLine());
+            } while (decision != 2);
+
+            break;
+        case 2:
+            do
+            {
+                Console.WriteLine("Ingrese dos numeros");
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+                resultado = num1 - num2;
+                Console.WriteLine("El resultado es:" + resultado);
+                Console.WriteLine("Desea realizar otra operacion? Ingrese 1 para si y ingrese 2 para No");
+                decision = int.Parse(Console.ReadLine());
+                if (decision == 2)
+                {
+                    opcion = 5;
+                }
+            } while (decision != 2);
+
+            break;
+        case 3:
+            do
+            {
+                Console.WriteLine("Ingrese dos numeros");
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+                resultado = num1 * num2;
+                Console.WriteLine("El resultado es:" + resultado);
+                Console.WriteLine("Desea realizar otra operacion? Ingrese 1 para si y ingrese 2 para No");
+                decision = int.Parse(Console.ReadLine());
+                if (decision == 2)
+                {
+                    opcion = 5;
+                }
+            } while (decision != 2);
+
+            break;
+             case 4:
+            do
+            {
+                Console.WriteLine("Ingrese dos numeros");
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+                resultado = num1 / num2;
+                Console.WriteLine("El resultado es:" + resultado);
+                Console.WriteLine("Desea realizar otro calculo? Ingrese 1 para si y ingrese 2 para No");
+                decision = int.Parse(Console.ReadLine());
+            } while (decision != 2);
+
+            break;
+
+
     }
-    else
-    {
-        Console.WriteLine("El numero debe ser mayor a 0");
-    }
-}
-else
-{
-    Console.WriteLine("No ingreso un numero");
-}
+    
+} while (opcion != 5);
